7408d044d2fde07c840ef59faf9da6a69936c012
Tony <piper1235@yahoo.com>
2017-07-24 14:06:35 -0700

    at 9 coredata

---
+++ Cubes/CubeViewController.swift
@@ -0,0 +1,56 @@
+//
+//  CubeViewController.swift
+//  Cubes
+//
+//  Created by Tony on 7/24/17.
+//  Copyright © 2017 Anthony Vitrano. All rights reserved.
+//
+
+import UIKit
+
+class CubeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var cubeImageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+    }
+
+ 
+    
+    @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        cubeImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let cube = Cube(context: context)
+        cube.title = titleTextField.text
+        cube.image = UIImagePNGRepresentation(cubeImageView.image!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+    }
+}
